d5ae8db9d723efcdecc8607f631ce756cb5e1698
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2017-03-17 16:37:21 +0000

    Integrate MLD into node-bindings

--- test/nodejs/osrm-data-path.js
+++ test/nodejs/osrm-data-path.js
@@ -2,7 +2,11 @@ var path = require('path');
 
 if (process.env.OSRM_DATA_PATH !== undefined) {
     exports.data_path = path.join(path.resolve(process.env.OSRM_DATA_PATH), "berlin_CH.osrm");
+    exports.mld_data_path = path.join(path.resolve(process.env.OSRM_DATA_PATH), "berlin_MLD.osrm");
+    exports.corech_data_path = path.join(path.resolve(process.env.OSRM_DATA_PATH), "berlin_CoreCH.osrm");
     console.log('Setting custom data path to ' + exports.data_path);
 } else {
     exports.data_path = path.resolve(path.join(__dirname, "../data/berlin_CH.osrm"));
+    exports.mld_data_path = path.resolve(path.join(__dirname, "../data/berlin_MLD.osrm"));
+    exports.corech_data_path = path.resolve(path.join(__dirname, "../data/berlin_CoreCH.osrm"));
 }
